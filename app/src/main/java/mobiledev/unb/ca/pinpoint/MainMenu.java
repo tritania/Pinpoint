1a1c51db6a2214cbb51b135f769da5323ec63e39
Erik wilson <erikwilson@magnorum.com>
2016-02-04 17:21:04 -0400

    Added some basic camera functions.

--- app/src/main/java/mobiledev/unb/ca/pinpoint/MainMenu.java
+++ app/src/main/java/mobiledev/unb/ca/pinpoint/MainMenu.java
@@ -1,6 +1,8 @@
 package mobiledev.unb.ca.pinpoint;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -22,6 +24,7 @@ public class MainMenu extends AppCompatActivity {
     Socket sock;
     Button playbutton;
     Button settingsbutton;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
 
     private static final String DEBUG = "Connection Response: ";
 
@@ -32,10 +35,13 @@ public class MainMenu extends AppCompatActivity {
                 @Override
                 public void run() {
                     Log.v(DEBUG, args[0].toString());
-                    if (args[0].toString().equals("LFG")) { //change fragment to waiting
+                    if (args[0].toString().equals("LFG")) { //change activity to waiting
 
                     } else if (args[0].toString().equals("TP")) { //prompt camera
-
+                        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+                        }
                     } else if (args[0].toString().equals("WFI")) { //go to map
                         Intent intent = new Intent(MainMenu.this, Map.class);
                         startActivity(intent);
@@ -44,6 +50,13 @@ public class MainMenu extends AppCompatActivity {
             });
         }
     };
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
